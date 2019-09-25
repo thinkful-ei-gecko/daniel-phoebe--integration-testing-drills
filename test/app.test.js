64423ebf738f8c7093c4ef302bc5807467572521
Daniel <herrnichols@gmail.com>
2019-09-25 16:03:38 -0400

    deleted leftover code in app.test.js. All tests that should pass are passing.

--- test/app.test.js
+++ test/app.test.js
@@ -1,41 +1,24 @@
 'use strict';
 
-// const sort = require('../app');
+const sort = require('../app');
 const app = require('../app');
-// const expect = require('chai').expect;
+const expect = require('chai').expect;
 const supertest = require('supertest');
 
-// describe('Sort function', () => {
-//   const arr = [4, 1, 3, 6];
-//   it('Should return all original values',() => {
-//     const answer = sort(arr);
-//     expect(answer).to.include.members(arr);
-//   });
-
-//   it('sorts in ascending order', () => {
-//     const answer = sort(arr);
-//     expect(answer).to.deep.equal([1, 3, 4, 6]);
-//   });
-
-//   it('contains all original values', () => {
-//     const answer = sort(arr);
-//     expect(answer).to.have.members(arr);
-//   });
-// });
-
-describe('Express app', () => {
-  it('should return a message from GET /', () => {
-    return supertest(app)
-      .get('/')
-      .expect(200, 'Hello Express');
+describe('Sort function', () => {
+  const arr = [4, 1, 3, 6];
+  it('Should return all original values',() => {
+    const answer = sort(arr);
+    expect(answer).to.include.members(arr);
   });
 
-  //it should return an object
+  it('sorts in ascending order', () => {
+    const answer = sort(arr);
+    expect(answer).to.deep.equal([1, 3, 4, 6]);
+  });
 
-  //it's count key should return the number of unique  
-  //characters
-
-  //it should return a 400 error if the input is an empty
-  //string 
-
+  it('contains all original values', () => {
+    const answer = sort(arr);
+    expect(answer).to.have.members(arr);
+  });
 });
